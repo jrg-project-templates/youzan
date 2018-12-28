bbac0f8896137ba51ac5b6f6ffd6aa09475dbc5a
miracletjf <1398988546@qq.com>
2018-12-28 23:07:34 +0800

    完善 foot.vue，图标可以根据路径切换状态

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,35 +1,79 @@
 <template>
   <div class="bottom-nav">
     <ul>
-      <li class="active">
-        <a href="/">
-          <i class="icon-home"></i>
+      <li v-for="(list,index) in footerLists" 
+        :class="{active: index === activeIndex}" 
+        :key="list.id" >
+        <a :href="list.href">
+          <i :class="list.icon"></i>
           <div>有赞</div>
-        </a>
-      </li>
-      <li>
-        <a href="./category.html">
-          <i class="icon-category"></i>
-          <div>分类</div>
-        </a>
-      </li>
-      <li>
-        <a href="https://h5.youzan.com/v2/trade/cart?f_platform=yzapp&amp;source=yzapp">
-          <i class="icon-cart"></i>
-          <div>购物车</div>
-        </a>
-      </li>
-      <li>
-        <a href="https://h5.youzan.com/v2/buyer/member">
-          <i class="icon-user"></i>
-          <div>我</div>
         </a>
       </li>
     </ul>
   </div>
 </template>
 
-<script> </script>
+<script>
+  export default {
+    data(){
+      return {
+        activeIndex: -1,
+        footerLists: [
+          {
+            id: 'footer-01',
+            name: '有赞',
+            icon: 'icon-home',
+            href: '/'
+          },
+          {
+            id: 'footer-02',
+            name: '分类',
+            icon: 'icon-category',
+            href: '/category.html'
+          },
+          {
+            id: 'footer-03',
+            name: '购物车',
+            icon: 'icon-cart',
+            href: ''
+          },
+          {
+            id: 'footer-04',
+            name: '我',
+            icon: 'icon-user',
+            href: ''
+          }
+        ]
+      }
+    },
+    created(){
+      this.getActiveListIndex();
+    },
+    methods: {
+      getUrl(){
+        let url = window.location.href;
+        let urlNoHttp = url.split('//')[1];
+        console.log(JSON.stringify(urlNoHttp));
+        let subUrlArrs = urlNoHttp.split('/');
+        subUrlArrs.shift();
+        return '/' + subUrlArrs.join('/').split('?')[0];
+      },
+      getActiveListIndex(){
+        let activeUrl = this.getUrl();
+        
+        this.footerLists.forEach((item,index)=>{
+          if(this.activeIndex > -1){
+            return
+          }
+          console.log(activeUrl);
+          if(item.href === activeUrl){
+            this.activeIndex = index;
+          }
+        })
+      }
+    }
+  }
+</script>
 
 <style>
 

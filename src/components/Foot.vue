125877cf0909d7e1ad51da7e4b9465da48d03066
miracletjf <1398988546@qq.com>
2019-01-03 00:41:12 +0800

    新增 cart,member,search 页面，更改 foot 组件

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="bottom-nav">
     <ul>
-      <li v-for="(list,index) in footerLists" 
+      <li v-for="(list,index) in navConfig" 
         :class="{active: index === activeIndex}" 
+        @click="changeNav(list,index)"
         :key="list.id" >
-        <a :href="list.href">
+        <a>
           <i :class="list.icon"></i>
           <div>有赞</div>
         </a>
@@ -14,62 +15,41 @@
 </template>
 
 <script>
+
+  import qs from 'qs'
+
+  let navConfig = [{
+      id: 'footer-01',
+      name: '有赞',
+      icon: 'icon-home',
+      href: '/'
+    }, {
+      id: 'footer-02',
+      name: '分类',
+      icon: 'icon-category',
+      href: 'category.html'
+    }, {
+      id: 'footer-03',
+      name: '购物车',
+      icon: 'icon-cart',
+      href: 'cart.html'
+    }, {
+      id: 'footer-04',
+      name: '我',
+      icon: 'icon-user',
+      href: 'member.html'
+    }]
+
   export default {
     data(){
       return {
-        activeIndex: -1,
-        footerLists: [
-          {
-            id: 'footer-01',
-            name: '有赞',
-            icon: 'icon-home',
-            href: '/'
-          },
-          {
-            id: 'footer-02',
-            name: '分类',
-            icon: 'icon-category',
-            href: '/category.html'
-          },
-          {
-            id: 'footer-03',
-            name: '购物车',
-            icon: 'icon-cart',
-            href: ''
-          },
-          {
-            id: 'footer-04',
-            name: '我',
-            icon: 'icon-user',
-            href: ''
-          }
-        ]
+        navConfig,
+        activeIndex: parseInt(qs.parse(location.search.slice(1)).index) || 0
       }
     },
-    created(){
-      this.getActiveListIndex();
-    },
     methods: {
-      getUrl(){
-        let url = window.location.href;
-        let urlNoHttp = url.split('//')[1];
-        console.log(JSON.stringify(urlNoHttp));
-        let subUrlArrs = urlNoHttp.split('/');
-        subUrlArrs.shift();
-        return '/' + subUrlArrs.join('/').split('?')[0];
-      },
-      getActiveListIndex(){
-        let activeUrl = this.getUrl();
-        
-        this.footerLists.forEach((item,index)=>{
-          if(this.activeIndex > -1){
-            return
-          }
-          console.log(activeUrl);
-          if(item.href === activeUrl){
-            this.activeIndex = index;
-          }
-        })
+      changeNav(list,index){
+        location.href = `${list.href}?index=${index}`;
       }
     }
   }
